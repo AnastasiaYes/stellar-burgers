08f182bf6850b0bc817b354575f5fd05c2a47544
Anastasia Esko <nastyho00@mail.ru>
2024-10-09 15:16:51 +0300

    writing a router in app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,129 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
-import styles from './app.module.css';
-
 import { AppHeader } from '@components';
+// import { useEffect } from 'react';
+// import { getAllIngredients } from '../../services/ingredientSlice/ingredientsSlice';
+// import { getUser } from '../../services/userSlice/userSlice';
+import styles from './app.module.css';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from '../../services/store';
+import { IngredientDetails, Modal, OrderInfo, FeedInfo } from '@components';
+import { ProtectedRoute } from '../protected-route/protected-route';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const backgroundLocation = location.state?.background;
+  const goBack = () => navigate(-1);
+  //
+  // useEffect(() => {
+  //   dispatch(getAllIngredients());
+  //   dispatch(getUser());
+  // }, [dispatch]);
+
+  return (
+    <>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes location={backgroundLocation || location}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route
+            path='login'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='register'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='forgot-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ForgotPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='reset-password'
+            element={
+              <ProtectedRoute onlyUnAuth>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='profile/orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='/feed/:number' element={<OrderInfo />} />
+          <Route path='/ingredients/:id' element={<IngredientDetails />} />
+          <Route path='/profile/orders/:number' element={<OrderInfo />} />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+
+        {backgroundLocation && (
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={
+                <Modal title={''} onClose={goBack}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <Modal title='Детали ингредиента' onClose={goBack}>
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <Modal title={''} onClose={goBack}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Routes>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default App;
