85887d1d1cdbcdebbc7b600b69146c6c261ba662
Anastasia Esko <nastyho00@mail.ru>
2024-10-15 11:57:53 +0300

    fix order-info and profile-order

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,21 +1,29 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useDispatch, useSelector } from '../../services/store';
+import { useParams } from 'react-router-dom';
+import { clearOrderData, fetchOrderByNumber, selectOrderData } from '../../services/slices/orderSlice';
+import { selectIngredients } from '../../services/slices/ingredientSlice';
 
 export const OrderInfo: FC = () => {
   /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const orderData = useSelector((state) => selectOrderData(state.order));
+  const ingredients: TIngredient[] = useSelector((state) => selectIngredients(state.ingredient));
+  const dispatch = useDispatch();
+  const param = useParams();
+  const number = Number(param.number);
 
-  const ingredients: TIngredient[] = [];
+    useEffect(() => {
+      if (!orderData) {
+        dispatch(fetchOrderByNumber(number));
+        return () => {
+          dispatch(clearOrderData());
+        };
+      }
+    }, []);
+
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,19 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { getUserOrders } from '../../services/slices/userSlice';
+import { selectOrderData } from '../../services/slices/orderSlice';
 
 export const ProfileOrders: FC = () => {
   /** TODO: взять переменную из стора */
+  // const orders: TOrder[] = useSelector(( state ) => selectOrderData(state.order));
+  const dispatch = useDispatch();
   const orders: TOrder[] = [];
+
+  useEffect(() => {
+    dispatch(getUserOrders());
+  }, []);
 
   return <ProfileOrdersUI orders={orders} />;
 };
